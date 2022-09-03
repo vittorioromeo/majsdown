8f412c4650f2dcf1f47c7ce7692432e233ed6d20
Vittorio Romeo <vittorio.romeo@outlook.com>
2022-09-03 21:29:52 +0300

    Add PCH

---
+++ src/majsdown/pch.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <string_view>
+#include <string>
+#include <type_traits>
+#include <utility>
+
+#include <cassert>
+#include <cstddef>
