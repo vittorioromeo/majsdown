c1e749ff2e34b4b38d5f67123b4defc2d674cc75
Vittorio Romeo <vittorio.romeo@outlook.com>
2022-09-09 21:02:56 +0300

    Refactor converter internals

--- src/majsdown/converter.cpp
+++ src/majsdown/converter.cpp
@@ -111,6 +111,172 @@ private:
                            << "' directive (" << reason << ")\n";
     }
 
+    [[nodiscard]] bool process_statement(const std::size_t js_start_idx)
+    {
+        const std::optional<std::size_t> js_end_idx =
+            find_next('\n', js_start_idx);
+
+        if (!js_end_idx.has_value())
+        {
+            error_diagnostic_unterminated('$', "missing newline");
+            return false;
+        }
+
+        assert(js_end_idx.has_value());
+
+        _tmp_buffer.clear();
+        copy_range_to_tmp_buffer(js_start_idx, *js_end_idx);
+
+        _js_buffer.append(_tmp_buffer);
+        _js_buffer.append(1, '\n');
+
+        _curr_idx = *js_end_idx + 1;
+        return true;
+    }
+
+    [[nodiscard]] bool process_inline(
+        std::string& output_buffer, const std::size_t js_start_idx)
+    {
+        const std::optional<std::size_t> js_end_idx =
+            find_js_end_idx(js_start_idx);
+
+        if (!js_end_idx.has_value())
+        {
+            error_diagnostic_unterminated('{', "missing closing brace");
+            return false;
+        }
+
+        assert(js_end_idx.has_value());
+
+        _tmp_buffer.clear();
+        _tmp_buffer.append("majsdown_set_output(");
+        copy_range_to_tmp_buffer(js_start_idx, *js_end_idx);
+        _tmp_buffer.append(");");
+
+        const std::string_view null_terminated_js = _tmp_buffer;
+        _js_interpreter.interpret(output_buffer, null_terminated_js);
+
+        _curr_idx = *js_end_idx + 1;
+        return true;
+    }
+
+    [[nodiscard]] bool process_code_block(
+        std::string& output_buffer, const std::size_t js_start_idx)
+    {
+        const std::size_t real_js_start_idx = js_start_idx + 1;
+
+        const std::optional<std::size_t> js_end_idx =
+            find_js_end_idx(real_js_start_idx);
+
+        if (!js_end_idx.has_value())
+        {
+            error_diagnostic_unterminated('_', "missing closing brace");
+            return false;
+        }
+
+        assert(js_end_idx.has_value());
+
+        _curr_idx = *js_end_idx + 2;
+
+        const std::optional<char> backtick0 = peek(0);
+        const std::optional<char> backtick1 = peek(1);
+        const std::optional<char> backtick2 = peek(2);
+
+        if (backtick0 != '`' || backtick1 != '`' || backtick2 != '`')
+        {
+            error_diagnostic_unterminated('_', "expected ``` code block");
+            return false;
+        }
+
+        step_fwd(3);
+        assert(_curr_idx < _source.size());
+
+        const std::size_t lang_start_idx = _curr_idx;
+
+        const auto lang_end_idx = [&]() -> std::optional<std::size_t>
+        {
+            for (std::size_t i = lang_start_idx; i < _source.size(); ++i)
+            {
+                if (_source[i] == '\n')
+                {
+                    return i;
+                }
+            }
+
+            return std::nullopt;
+        }();
+
+        if (!lang_end_idx.has_value())
+        {
+            error_diagnostic_unterminated('_', "malformed ``` code block");
+            return false;
+        }
+
+        assert(lang_end_idx.has_value());
+
+        const std::string_view extracted_lang =
+            _source.substr(lang_start_idx, *lang_end_idx - lang_start_idx);
+
+        const std::size_t code_start_idx = *lang_end_idx + 1;
+
+        const auto code_end_idx = [&]() -> std::optional<std::size_t>
+        {
+            for (std::size_t i = code_start_idx; i < _source.size() - 3; ++i)
+            {
+                if (_source[i] == '\n' && _source[i + 1] == '`' &&
+                    _source[i + 2] == '`' && _source[i + 3] == '`')
+                {
+                    return i;
+                }
+            }
+
+            return std::nullopt;
+        }();
+
+        if (!code_end_idx.has_value())
+        {
+            error_diagnostic_unterminated('_', "open ``` code block");
+            return false;
+        }
+
+        assert(code_end_idx.has_value());
+
+        const std::string_view extracted_code =
+            _source.substr(code_start_idx, *code_end_idx - code_start_idx);
+
+        _tmp_buffer.clear();
+        _tmp_buffer.append("(() => { const code = String.raw`");
+        _tmp_buffer.append(extracted_code);
+        _tmp_buffer.append("`; const lang = `");
+        _tmp_buffer.append(extracted_lang);
+        _tmp_buffer.append("`; return majsdown_set_output(");
+        copy_range_to_tmp_buffer(real_js_start_idx, *js_end_idx);
+        _tmp_buffer.append("); })()");
+
+        const std::string_view null_terminated_js = _tmp_buffer;
+        _js_interpreter.interpret(output_buffer, null_terminated_js);
+
+        _curr_idx = *code_end_idx + 4;
+        return true;
+    }
+
+    void process_normal_character(std::string& output_buffer, const char c)
+    {
+        if (c == '\n')
+        {
+            ++_curr_line;
+            _js_interpreter.set_current_diagnostics_line(_curr_line);
+        }
+
+        output_buffer.append(1, c);
+        step_fwd(1);
+    }
+
+    [[nodiscard]] bool is_special_character(const char c)
+    {
+        return c == '$' || c == '{' || c == '_';
+    }
+
 public:
     [[nodiscard]] explicit converter_impl(const std::string_view source)
         : _source{source}, _curr_idx{0}, _curr_line{0}
@@ -129,46 +295,27 @@ public:
 
             if (c != '@')
             {
-                if (c == '\n')
-                {
-                    ++_curr_line;
-                    _js_interpreter.set_current_diagnostics_line(_curr_line);
-                }
-
-                output_buffer.append(1, c);
-                step_fwd(1);
-
+                process_normal_character(output_buffer, c);
                 continue;
             }
 
             assert(c == '@');
 
+            if (const std::optional<char> next1 = peek(1);
+                !next1.has_value() || *next1 != '@')
             {
-                const std::optional<char> next1 = peek(1);
-
-                if (!next1.has_value() || *next1 != '@')
-                {
-                    output_buffer.append(1, c);
-                    step_fwd(1);
-
-                    continue;
-                }
-
-                assert(*next1 == '@');
-            }
-
-            const std::optional<char> next2 = peek(2);
-
-            if (!next2.has_value() ||
-                (*next2 != '$' && *next2 != '{' && *next2 != '_'))
-            {
-                output_buffer.append(1, c);
-                step_fwd(1);
-
+                process_normal_character(output_buffer, c);
                 continue;
             }
 
-            assert(*next2 == '$' || *next2 == '{' || *next2 == '_');
+            const std::optional<char> next2 = peek(2);
+            if (!next2.has_value() || !is_special_character(*next2))
+            {
+                process_normal_character(output_buffer, c);
+                continue;
+            }
+
+            assert(is_special_character(*next2));
 
             const std::size_t js_start_idx = _curr_idx + 3;
             if (js_start_idx >= _source.size())
@@ -184,24 +331,11 @@ public:
             // ----------------------------------------------------------------
             if (*next2 == '$')
             {
-                const std::optional<std::size_t> js_end_idx =
-                    find_next('\n', js_start_idx);
-
-                if (!js_end_idx.has_value())
+                if (!process_statement(js_start_idx))
                 {
-                    error_diagnostic_unterminated(*next2, "missing newline");
                     return false;
                 }
 
-                assert(js_end_idx.has_value());
-
-                _tmp_buffer.clear();
-                copy_range_to_tmp_buffer(js_start_idx, *js_end_idx);
-
-                _js_buffer.append(_tmp_buffer);
-                _js_buffer.append(1, '\n');
-
-                _curr_idx = *js_end_idx + 1;
                 continue;
             }
             else if (!_js_buffer.empty())
@@ -215,28 +349,11 @@ public:
             // ----------------------------------------------------------------
             if (*next2 == '{')
             {
-                const std::optional<std::size_t> js_end_idx =
-                    find_js_end_idx(js_start_idx);
-
-                if (!js_end_idx.has_value())
+                if (!process_inline(output_buffer, js_start_idx))
                 {
-                    error_diagnostic_unterminated(
-                        *next2, "missing closing brace");
-
                     return false;
                 }
 
-                assert(js_end_idx.has_value());
-
-                _tmp_buffer.clear();
-                _tmp_buffer.append("majsdown_set_output(");
-                copy_range_to_tmp_buffer(js_start_idx, *js_end_idx);
-                _tmp_buffer.append(");");
-
-                const std::string_view null_terminated_js = _tmp_buffer;
-                _js_interpreter.interpret(output_buffer, null_terminated_js);
-
-                _curr_idx = *js_end_idx + 1;
                 continue;
             }
 
@@ -245,110 +362,11 @@ public:
             // ----------------------------------------------------------------
             if (*next2 == '_')
             {
-                const std::size_t real_js_start_idx = js_start_idx + 1;
-
-                const std::optional<std::size_t> js_end_idx =
-                    find_js_end_idx(real_js_start_idx);
-
-                if (!js_end_idx.has_value())
+                if (!process_code_block(output_buffer, js_start_idx))
                 {
-                    error_diagnostic_unterminated(
-                        *next2, "missing closing brace");
-
                     return false;
                 }
 
-                assert(js_end_idx.has_value());
-
-                _curr_idx = *js_end_idx + 2;
-
-                const std::optional<char> backtick0 = peek(0);
-                const std::optional<char> backtick1 = peek(1);
-                const std::optional<char> backtick2 = peek(2);
-
-                if (backtick0 != '`' || backtick1 != '`' || backtick2 != '`')
-                {
-                    error_diagnostic_unterminated(
-                        *next2, "expected ``` code block");
-
-                    return false;
-                }
-
-                step_fwd(3);
-                assert(_curr_idx < _source.size());
-
-                const std::size_t lang_start_idx = _curr_idx;
-
-                const auto lang_end_idx = [&]() -> std::optional<std::size_t>
-                {
-                    for (std::size_t i = lang_start_idx; i < _source.size();
-                         ++i)
-                    {
-                        if (_source[i] == '\n')
-                        {
-                            return i;
-                        }
-                    }
-
-                    return std::nullopt;
-                }();
-
-                if (!lang_end_idx.has_value())
-                {
-                    error_diagnostic_unterminated(
-                        *next2, "malformed ``` code block");
-
-                    return false;
-                }
-
-                assert(lang_end_idx.has_value());
-
-                const std::string_view extracted_lang = _source.substr(
-                    lang_start_idx, *lang_end_idx - lang_start_idx);
-
-                const std::size_t code_start_idx = *lang_end_idx + 1;
-
-                const auto code_end_idx = [&]() -> std::optional<std::size_t>
-                {
-                    for (std::size_t i = code_start_idx; i < _source.size() - 3;
-                         ++i)
-                    {
-                        if (_source[i] == '\n' && _source[i + 1] == '`' &&
-                            _source[i + 2] == '`' && _source[i + 3] == '`')
-                        {
-                            return i;
-                        }
-                    }
-
-                    return std::nullopt;
-                }();
-
-                if (!code_end_idx.has_value())
-                {
-                    error_diagnostic_unterminated(
-                        *next2, "open ``` code block");
-
-                    return false;
-                }
-
-                assert(code_end_idx.has_value());
-
-                const std::string_view extracted_code = _source.substr(
-                    code_start_idx, *code_end_idx - code_start_idx);
-
-                _tmp_buffer.clear();
-                _tmp_buffer.append("(() => { const code = String.raw`");
-                _tmp_buffer.append(extracted_code);
-                _tmp_buffer.append("`; const lang = `");
-                _tmp_buffer.append(extracted_lang);
-                _tmp_buffer.append("`; return majsdown_set_output(");
-                copy_range_to_tmp_buffer(real_js_start_idx, *js_end_idx);
-                _tmp_buffer.append("); })()");
-
-                const std::string_view null_terminated_js = _tmp_buffer;
-                _js_interpreter.interpret(output_buffer, null_terminated_js);
-
-                _curr_idx = *code_end_idx + 4;
                 continue;
             }
 
